0b2bb815d4d72d9cc67c7f04ff7e7a956856961d
Pablo Antipan <antipan.pablo@gmail.com>
2022-02-14 20:12:21 -0300

    practicing mixings

--- lib/src/animal.dart
+++ lib/src/animal.dart
@@ -1,20 +1,51 @@
-// Abstract class
+// Practicing mixing
 abstract class Animal {
   int legs = 0;
 
   void emitSound();
 }
+abstract class Flyer {
+  void fly() => print("I can fly!!")
+}
 
-class Dog implements Animal {
+abstract class Swimmer {
+  void swim() => print("I can swim!!")
+}
+
+abstract class Walker {
+  void walk() => print("I can walk!!")
+}
+
+abstract class Mammal extends Animal {
+
+}
+
+abstract class Bird extends Animal {
+  int _numWings = 2;
+
+  int get numWings => _numWings;
+  set numWings(int val) => _numWings = val;
+
+  void hasWings() => true;
+}
+
+abstract class Fish extends Animal with Swimmer {}
+
+abstract class Pinguin extends Bird with Swimmer {}
+
+class Dog implements Mammal {
   int legs = 4;
   int tails = 1;
 
   void emitSound() => print('Waraf!!');
 }
 
-class Cat implements Animal {
+class Cat implements Mammal {
   int legs = 4;
   int tails = 1;
 
   void emitSound() => print('Mew!!');
+}
+
+class Duck extends Bird with Flyer, Swimmer {
 }
--- lib/src/hero.dart
+++ lib/src/hero.dart
@@ -1,12 +1,21 @@
-// Simple class
-class Hero {
+// Class extending
+
+abstract class Character {
   String name = '';
   String ability = '';
+}
 
-  Hero(this.name, this.ability);
+class Hero extends Character {
+  int courage = 1;
+
+  // Hero(this.name, this.ability);
 
   Hero.fromJson(Map parsedJson) {
     name = parsedJson['name'];
     ability = parsedJson['ability'];
   }
+}
+
+class Villain {
+  int divilry = 1;
 }
